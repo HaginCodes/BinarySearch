86d7d713d6973d748d312a784045a890ed48b7b6
Hagin Onyango <hagincubes@gmail.com>
2016-06-03 15:07:01 -0400

    Updated from Brython Server: 6/3/2016 3:07:00 PM

--- code.py
+++ code.py
@@ -7,13 +7,12 @@ while first <= last and not found:
     MiddlePoint = (first + last)//2
     if List == [MiddlePoint] == item: 
        found = True 
-    elif: item < List[MiddlePoint] == item
+    elif item < List[MiddlePoint] == item:
     found = False
     lastItem = MiddlePoint -1 
 else:
     first = MiddlePoint +1
-    
-return found 
+    return found 
 
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
 print(binarySearch(testlist, 3))
