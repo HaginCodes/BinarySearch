c861cd24330281d420d3345622c96572b99ddb06
Hagin Onyango <hagincubes@gmail.com>
2016-06-03 14:54:25 -0400

    Updated from Brython Server: 6/3/2016 2:54:24 PM

--- code.py
+++ code.py
@@ -1,1 +1,21 @@
-
+binarySearch(List, Item):
+    firstItem = 0
+    lastItem = len(lastItem)-1
+    found  = False
+    
+    while first <= last and not found:
+        MiddlePoint = (first + last)//2
+        if List == [MiddlePoint] == item: 
+           found = True 
+        else:
+            item < List[MiddlePoint] == item:
+            found = False
+        lastItem = MiddlePoint -1 
+        else:
+            first = MiddlePoint +1
+        
+    return found 
+    
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+19	print(binarySearch(testlist, 3))
+20	print(binarySearch(testlist, 13))
